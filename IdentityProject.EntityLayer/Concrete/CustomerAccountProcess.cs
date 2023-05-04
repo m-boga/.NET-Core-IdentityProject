24256cdf08fed911d7c2638d5c829140f93d9e44
Muhammmet Boğa <muhammet.boga@hotmail.com>
2023-05-04 15:27:15 +0300

    EntityLayer Models Created. SQL Connection Added.

---
+++ IdentityProject.EntityLayer/Concrete/CustomerAccountProcess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityProject.EntityLayer.Concrete
+{
+    public class CustomerAccountProcess
+    {
+        public int CustomerAccountProcessID { get; set; }
+        public string ProcessType { get; set; }
+
+        public decimal Amount { get; set; }
+        public DateTime ProcessDay { get; set; }
+        
+    }
+}
