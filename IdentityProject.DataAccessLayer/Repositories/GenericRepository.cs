c6a62f1d1c18830eb554476244593d6f074631d9
Muhammmet Boğa <muhammet.boga@hotmail.com>
2023-05-12 23:48:59 +0300

    Interface and Repositories were Created.

---
+++ IdentityProject.DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,41 @@
+﻿using IdentityProject.DataAccessLayer.Abstract;
+using IdentityProject.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Xml;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityProject.DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public void Delete(T t)
+        {
+            using var context = new Context();
+            context.Set<T>().Remove(t);
+            context.SaveChanges();
+        }
+
+        public T GetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(T t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
