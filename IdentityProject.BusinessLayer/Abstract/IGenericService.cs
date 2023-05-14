4802937e68c940bcb652eb074195d0d4ee6f55a5
Muhammmet Boğa <muhammet.boga@hotmail.com>
2023-05-14 23:22:08 +0300

    comment lines added.

---
+++ IdentityProject.BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityProject.BusinessLayer.Abstract
+{
+    public interface IGenericService<T> where T : class
+    {
+    }
+}
--- IdentityProject.DataAccessLayer/Repositories/GenericRepository.cs
+++ IdentityProject.DataAccessLayer/Repositories/GenericRepository.cs
@@ -9,6 +9,16 @@ using System.Threading.Tasks;
 
 namespace IdentityProject.DataAccessLayer.Repositories
 {
+    /*
+     * Bu kod satırı, "GenericRepository" adında bir sınıfı tanımlar. 
+     * Sınıf, "IGenericDal" arayüzünü uygular ve "T" tipinde bir parametre alır. 
+     * "where T : class" ifadesiyle, "T" parametresinin referans tip bir sınıf olması gerektiği belirtilir. 
+     * Yani, "T" herhangi bir sınıf tipi olabilir, ancak değer tiplerini kabul etmez.
+
+        Bu yapı, bir genel veritabanı işlem sınıfı tasarlamak için kullanılabilir.
+        "T" parametresi, farklı sınıfların veritabanı işlemlerini gerçekleştirmek için kullanılan tipi temsil eder.
+        Bu sayede, aynı kodu farklı tipler için tekrar tekrar yazmak yerine, 
+        genel bir sınıf kullanarak tekrar kullanılabilirlik sağlanır.*/
     public class GenericRepository<T> : IGenericDal<T> where T : class
     {
         public void Delete(T t)
@@ -18,24 +28,34 @@ namespace IdentityProject.DataAccessLayer.Repositories
             context.SaveChanges();
         }
 
+        // Belirli bir kimliğe sahip veriyi veritabanından alır
         public T GetByID(int id)
         {
-            throw new NotImplementedException();
+            using var context = new Context();
+            return context.Set<T>().Find(id);
         }
-
+        
+        // Tüm verileri veritabanından alır ve bir liste olarak döndürür
         public List<T> GetList()
         {
-            throw new NotImplementedException();
+            using var context = new Context();
+            return context.Set<T>().ToList();
         }
 
+        // Yeni bir veri ekler
         public void Insert(T t)
         {
-            throw new NotImplementedException();
+            using var context = new Context();
+            context.Set<T>().Add(t);
+            context.SaveChanges();
         }
 
+        // Var olan bir veriyi günceller
         public void Update(T t)
         {
-            throw new NotImplementedException();
+            using var context = new Context();
+            context.Set<T>().Update(t);
+            context.SaveChanges();
         }
     }
 }
